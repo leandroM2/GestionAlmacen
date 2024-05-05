e064f40ff582c7cd80686204d39c134569a940ab
leandroM2 <l_jmm@hotmail.com>
2024-05-04 19:33:08 -0500

    carpeta springboot

---
+++ com.inn.almacen/src/main/java/com/inn/almacen/GestionAlmacenApplication.java
@@ -0,0 +1,13 @@
+package com.inn.almacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAlmacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAlmacenApplication.class, args);
+	}
+
+}
---
+++ com.inn.almacen/src/test/java/com/inn/almacen/GestionAlmacenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inn.almacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAlmacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
