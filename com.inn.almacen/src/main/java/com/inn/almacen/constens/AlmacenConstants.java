2c59b82fb69fe9d58e5dccf507366e3de4bc501c
leandroM2 <l_jmm@hotmail.com>
2024-06-30 00:43:31 -0500

    constantes para documento pdf

--- com.inn.almacen/src/main/java/com/inn/almacen/constens/AlmacenConstants.java
+++ com.inn.almacen/src/main/java/com/inn/almacen/constens/AlmacenConstants.java
@@ -2,7 +2,9 @@ package com.inn.almacen.constens;
 
 public class AlmacenConstants {
 
-    public static final String ALGO_SALIO_MAL="Algo salió mal.";
-    public static final String DATA_INVALIDA="Data no válida.";
-    public static final String ACCESO_NO_AUTORIZADO="Acceso no autorizado.";
+    public static final String ALGO_SALIO_MAL="ALGO SALIÓ MAL.";
+    public static final String DATA_INVALIDA="DATA NO ES VÁLIDA.";
+    public static final String ACCESO_NO_AUTORIZADO="SU USUARIO NO CUENTA CON LOS PERMISOS NECESARIOS.";
+    public static final String RUTA_ORDEN_COMPRA_ORDEN="F:\\Repositorio\\com.inn.almacen\\src\\main\\resources\\templates\\report\\Orden.jrxml";
+    public static final String RUTA_ORDEN_COMPRA_PDF="F:\\Repositorio\\com.inn.almacen\\src\\main\\resources\\templates\\orders\\";
 }
